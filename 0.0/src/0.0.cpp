e19224d40166b30822bf95a2e1c7db73d8c743d3
LeoggYY <153478805+LeoggYY@users.noreply.github.com>
2024-12-11 22:41:10 +0800

    Update 0.0.cpp

--- 0.0/src/0.0.cpp
+++ 0.0/src/0.0.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 class Polynomial;
-
 
 class Term {
     friend class Polynomial;
@@ -15,14 +13,13 @@ class Term {
 public:
     Term(float c = 0.0f, int e = 0) : coef(c), exp(e) {}
 private:
-    float coef;  
-    int exp;     
+    float coef;
+    int exp;
 };
-
 
 class Polynomial {
 public:
-    Polynomial() {}
+    Polynomial() : termCount(0) {}
 
     Polynomial Add(const Polynomial& poly) const;
     Polynomial Mult(const Polynomial& poly) const;
@@ -32,139 +29,111 @@ public:
     friend ostream& operator<<(ostream& os, const Polynomial& poly);
 
 private:
-    vector<Term> terms;  //¶µ
-
+    static const int MAX_TERMS = 100;
+    Term terms[MAX_TERMS];
+    int termCount;
     void sortTerms();
 };
 
-
 void Polynomial::sortTerms() {
-    sort(terms.begin(), terms.end(), [](const Term& a, const Term& b) {
+    sort(terms, terms + termCount, [](const Term& a, const Term& b) {
         return a.exp > b.exp;
-        });
+    });
 }
 
-// ¥[ªk
 Polynomial Polynomial::Add(const Polynomial& poly) const {
     Polynomial result;
     int i = 0, j = 0;
-
-    while (i < terms.size() && j < poly.terms.size()) {
+    while (i < termCount && j < poly.termCount) {
         if (terms[i].exp == poly.terms[j].exp) {
             float sumCoef = terms[i].coef + poly.terms[j].coef;
-            if (sumCoef != 0) {
-                result.terms.push_back(Term(sumCoef, terms[i].exp));
+            if (sumCoef != 0 && result.termCount < MAX_TERMS) {
+                result.terms[result.termCount++] = Term(sumCoef, terms[i].exp);
             }
-            i++;
-            j++;
-        }
-        else if (terms[i].exp > poly.terms[j].exp) {
-            result.terms.push_back(terms[i++]);
-        }
-        else {
-            result.terms.push_back(poly.terms[j++]);
+            i++; j++;
+        } else if (terms[i].exp > poly.terms[j].exp) {
+            result.terms[result.termCount++] = terms[i++];
+        } else {
+            result.terms[result.termCount++] = poly.terms[j++];
         }
     }
-
-    while (i < terms.size()) {
-        result.terms.push_back(terms[i++]);
-    }
-
-    while (j < poly.terms.size()) {
-        result.terms.push_back(poly.terms[j++]);
-    }
-
+    while (i < termCount) result.terms[result.termCount++] = terms[i++];
+    while (j < poly.termCount) result.terms[result.termCount++] = poly.terms[j++];
     result.sortTerms();
     return result;
 }
 
-// ­¼ªk
 Polynomial Polynomial::Mult(const Polynomial& poly) const {
     Polynomial result;
-
-    for (int i = 0; i < terms.size(); ++i) {
-        for (int j = 0; j < poly.terms.size(); ++j) {
+    for (int i = 0; i < termCount; ++i) {
+        for (int j = 0; j < poly.termCount; ++j) {
             float prodCoef = terms[i].coef * poly.terms[j].coef;
             int prodExp = terms[i].exp + poly.terms[j].exp;
-
             bool found = false;
-            for (int k = 0; k < result.terms.size(); ++k) {
+            for (int k = 0; k < result.termCount; ++k) {
                 if (result.terms[k].exp == prodExp) {
                     result.terms[k].coef += prodCoef;
                     found = true;
                     break;
                 }
             }
-
-            if (!found) {
-                result.terms.push_back(Term(prodCoef, prodExp));
+            if (!found && result.termCount < MAX_TERMS) {
+                result.terms[result.termCount++] = Term(prodCoef, prodExp);
             }
         }
     }
-
     result.sortTerms();
     return result;
 }
 
 float Polynomial::Eval(float x) const {
     float result = 0;
-    for (int i = 0; i < terms.size(); ++i) {
+    for (int i = 0; i < termCount; ++i) {
         result += terms[i].coef * pow(x, terms[i].exp);
     }
     return result;
 }
 
-
 istream& operator>>(istream& is, Polynomial& poly) {
-    int n;
-    cout << "¦³´X¶µ:";
-    is >> n;
-    for (int i = 0; i < n; ++i) {
-        float coef;
-        int exp;
-        cout << "¿é¤J²Ä " << i + 1 << " ¶µªº«Y¼Æ»P«ü¼Æ: ";
+    cout << "æœ‰å¹¾é …: ";
+    is >> poly.termCount;
+    for (int i = 0; i < poly.termCount; ++i) {
+        float coef; int exp;
+        cout << "è¼¸å…¥ç¬¬ " << i + 1 << " é …çš„ä¿‚æ•¸èˆ‡æŒ‡æ•¸: ";
         is >> coef >> exp;
-        poly.terms.push_back(Term(coef, exp));
+        poly.terms[i] = Term(coef, exp);
     }
+    poly.sortTerms();
     return is;
 }
 
-
 ostream& operator<<(ostream& os, const Polynomial& poly) {
     bool firstTerm = true;
-
-    for (int i = 0; i < poly.terms.size(); ++i) {
+    for (int i = 0; i < poly.termCount; ++i) {
         if (poly.terms[i].coef == 0) continue;
-
         if (!firstTerm && poly.terms[i].coef > 0) os << "+";
         os << poly.terms[i].coef << "x^" << poly.terms[i].exp << " ";
-
         firstTerm = false;
     }
-
     return os;
 }
 
 int main() {
     Polynomial p1, p2, sum, product;
-
-    cout << "¿é¤J²Ä¤@­Ó¦h¶µ¦¡:\n";
-    cin >> p1;
-    cout << "¿é¤J²Ä¤G­Ó¦h¶µ¦¡:\n";
-    cin >> p2;
+    cout << "è¼¸å…¥ç¬¬ä¸€å€‹å¤šé …å¼:\n"; cin >> p1;
+    cout << "è¼¸å…¥ç¬¬äºŒå€‹å¤šé …å¼:\n"; cin >> p2;
 
     sum = p1.Add(p2);
     product = p1.Mult(p2);
 
-    cout << "²Ä¤@­Ó¦h¶µ¦¡: " << p1 << endl;
-    cout << "²Ä¤G­Ó¦h¶µ¦¡: " << p2 << endl;
-    cout << "¥[ªkµ²ªG: " << sum << endl;
-    cout << "­¼ªkµ²ªG: " << product << endl;
+    cout << "ç¬¬ä¸€å€‹å¤šé …å¼: " << p1 << endl;
+    cout << "ç¬¬äºŒå€‹å¤šé …å¼: " << p2 << endl;
+    cout << "åŠ æ³•çµæžœ: " << sum << endl;
+    cout << "ä¹˜æ³•çµæžœ: " << product << endl;
 
     float x;
-    cout << "¿é¤J¤@­Ó­È±a¤J: ";
-    cin >> x;
-    cout << "P1(" << x << ") = " << p1.Eval(x) << endl;  
-    cout << "P2(" << x << ") = " << p2.Eval(x) << endl;  
+    cout << "è¼¸å…¥ä¸€å€‹å€¼å¸¶å…¥: "; cin >> x;
+    cout << "P1(" << x << ") = " << p1.Eval(x) << endl;
+    cout << "P2(" << x << ") = " << p2.Eval(x) << endl;
     return 0;
 }
